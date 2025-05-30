f4ced334e2737c1e85e1fe236e928aec6a4d658c
Fizza-Wajid <wfizzah@gmail.com>
2025-05-31 01:20:14 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,2 @@
-# hello.py
-
-"""
-This script prints 'Hello' when run directly.
-"""
-
-def main():
-    """Main function that prints Hello"""
-    print("Hello")
-
-
-if __name__ == "__main__":
-    main()
+def add(a, b):
+    return a + b
