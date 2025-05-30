5762692db5e293c52c255fc804ec84718afa7c05
Fizza-Wajid <wfizzah@gmail.com>
2025-05-30 18:51:59 +0500

    Update hello.py
    changed python file

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 # hello.py
 
+"""
+This script prints 'Hello' when run directly.
+"""
+
 def main():
+    """Main function that prints Hello"""
     print("Hello")
+
 
 if __name__ == "__main__":
     main()
